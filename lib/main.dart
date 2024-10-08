9311aba59070bf53f5c2b7debe3a3a3268f8dcd7
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-08 10:32:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:helloworld/ButtonSeletionWidget.dart';
 import 'package:helloworld/MyHttpOverrides.dart';
 import 'package:helloworld/MyPlusWidget.dart';
 import 'package:helloworld/MyStatePlusWidget.dart';
+import 'package:helloworld/models/Product.dart';
 
 TextEditingController txt1_controller = TextEditingController();
 TextEditingController txt2_controller = TextEditingController();
@@ -12,24 +13,6 @@ TextEditingController kq_controller = TextEditingController();
 
 void main() {
   HttpOverrides.global = new MyHttpOverrides();
-
-  final List<String> dssanpham = <String>[
-    'Sản phẩm 1',
-    'Sản phẩm 2',
-    'Sản phẩm 3',
-    'Sản phẩm 4',
-    'Sản phẩm 5',
-    'Sản phẩm 6',
-    'Sản phẩm 7',
-    'Sản phẩm 8',
-    'Sản phẩm 9',
-    'Sản phẩm 10',
-    'Sản phẩm 11',
-    'Sản phẩm 12',
-    'Sản phẩm 13',
-    'Sản phẩm 14',
-    'Sản phẩm 15',
-  ];
 
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -40,17 +23,25 @@ void main() {
                       'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
               body: Container(
                   margin: EdgeInsets.all(10.0),
-                  transform: Matrix4.rotationZ(0.03),
+                  transform: Matrix4.rotationZ(0.02),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20.0),
-                      color: Color(0xff6f82d0)),
+                      color: Color(0x85fd2408)),
                   child: ListView.builder(
-                      itemCount: dssanpham.length,
+                      itemCount: products.length,
                       itemBuilder: (BuildContext context, int index) {
                         return Container(
-                          height: 50,
-                          child: Text(
-                              'Ten san phảm:' + dssanpham[index].toString()),
-                        );
+                            height: 200,
+                            child: Container(
+                                height: 200,
+                                color: Color(0xffd5e0e8),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                        child: Image.asset(
+                                            products[index].image.toString())),
+                                    Text(products[index].title.toString()),
+                                  ],
+                                )));
                       }))))));
 }
