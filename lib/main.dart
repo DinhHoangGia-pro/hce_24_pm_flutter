4963ab1216c7c39f62e419707a6820c9d6044a20
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-08 09:08:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,19 @@ void main() {
               appBar: AppBar(
                   title: Image.network(
                       'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
-              body: ListView.builder(
-                  itemCount: dssanpham.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 50,
-                      child:
-                          Text('Ten san phảm:' + dssanpham[index].toString()),
-                    );
-                  })))));
+              body: Container(
+                  margin: EdgeInsets.all(10.0),
+                  transform: Matrix4.rotationZ(0.03),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Color(0xff6f82d0)),
+                  child: ListView.builder(
+                      itemCount: dssanpham.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 50,
+                          child: Text(
+                              'Ten san phảm:' + dssanpham[index].toString()),
+                        );
+                      }))))));
 }
