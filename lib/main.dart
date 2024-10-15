8606bc084dcc125541d66a853bdb7e7c91b94884
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-15 07:54:20 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,81 @@ import 'package:helloworld/MyPlusWidget.dart';
 import 'package:helloworld/MyStatePlusWidget.dart';
 import 'package:helloworld/models/Product.dart';
 
-TextEditingController txt1_controller = TextEditingController();
-TextEditingController txt2_controller = TextEditingController();
-TextEditingController kq_controller = TextEditingController();
-
 void main() {
   HttpOverrides.global = new MyHttpOverrides();
 
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
-          child: Scaffold(
-              appBar: AppBar(
-                  title: Image.network(
-                      'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
-              body: Container(
-                  margin: EdgeInsets.all(10.0),
-                  transform: Matrix4.rotationZ(0.02),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: Color(0x85fd2408)),
-                  child: ListView.builder(
-                      itemCount: products.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                            height: 200,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Image.network(
+                    'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
+
+            // Thay ListView.builder bằng GridView.builder
+            body: Container(
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Color(0xffcf83a9),
+              ),
+              child: GridView.builder(
+                  itemCount: products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Số cột là 2
+                    childAspectRatio: 0.75, // Tỷ lệ chiều cao / chiều rộng
+                    crossAxisSpacing: 10.0, // Khoảng cách giữa các cột
+                    mainAxisSpacing: 10.0, // Khoảng cách giữa các hàng
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 5.0, // Hiệu ứng đổ bóng cho sản phẩm
+                      child: Column(
+                        children: [
+                          Expanded(
                             child: Container(
-                                height: 200,
-                                color: Color(0xffd5e0e8),
-                                child: Column(
-                                  children: [
-                                    Expanded(
-                                        child: Image.asset(
-                                            products[index].image.toString())),
-                                    Text(products[index].title.toString()),
-                                  ],
-                                )));
-                      }))))));
+                              padding: EdgeInsets.all(5.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Hero(
+                                tag: "${products[index].id}",
+                                child: Image.asset(
+                                  products[index].image.toString(),
+                                  fit: BoxFit
+                                      .cover, // Đảm bảo hình ảnh phủ kín vùng
+                                ),
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            title: Text(
+                              products[index].title.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow
+                                  .ellipsis, // Cắt bớt text nếu quá dài
+                            ),
+                            subtitle: Text(
+                              "\$${products[index].price.toString()}",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            trailing: Icon(Icons
+                                .shopping_cart_outlined), // Biểu tượng giỏ hàng
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            )),
+      )));
 }
