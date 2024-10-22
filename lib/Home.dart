e241dc7f27fbe67e38e58314ae7f78b6062f19df
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-10-21 21:47:08 -0400

    Add files via upload

---
+++ lib/Home.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:helloworld/Hienthisanpham.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // Define pages for navigation
+  final List<Widget> _pages = [
+    Hienthisanpham(),
+    Hienthisanpham(),
+    Hienthisanpham(),
+  ];
+
+  // Function to handle tab switching
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Image.network(
+              'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
+      body: _pages[_selectedIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:helloworld/Hienthisanpham.dart';
 import 'package:helloworld/MyHttpOverrides.dart';
+import 'package:helloworld/Home.dart';
 
 void main() {
   HttpOverrides.global = new MyHttpOverrides();
 
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Scaffold(
-              appBar: AppBar(
-                  title: Image.network(
-                      'https://img.freepik.com/premium-vector/ho-chi-minh-city-vietnam-skyline-with-panorama-white-background-vector-illustration-business-travel-tourism-concept-with-modern-buildings-image-banner-web-site_596401-63.jpg?w=1060')),
-              body: Hienthisanpham()))));
+  runApp(Home());
 }
